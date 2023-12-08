04b389ee53b725969686680a9ad32f67f71c3692
Sean Mackay <mackaysean2@gmail.com>
2023-12-08 15:29:09 +0000

    Map, pins added

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { Marker, Callout } from 'react-native-maps'
+import { StyleSheet, View, Text, Alert } from 'react-native'
+import { Locations } from './locations'
+import { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [current_view, setCurrentView] = useState('map')
+    const Stack = createNativeStackNavigator();
+    
+    const initial_region = {
+        latitude: 55.8628075,
+        longitude: -4.2634654,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05
+    }
+
+    const changeView = (location) => {
+        setCurrentView(location.name)
+    }
+
+    const onMarkerPress = (location) => {
+        Alert.alert(location.name)
+        // changeView(location)
+    }
+
+    if (current_view == 'map') {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Welcome to the Test Map</Text>
+                <MapView
+                    style={styles.map}
+                    provider={PROVIDER_GOOGLE}
+                    region={initial_region}
+                    showsUserLocation
+                >
+                    {/* On Press function for marker pin*/}
+                    {/* {Locations.map((location, index) => (
+                        <Marker key={index} coordinate={location.coordinate} onPress={() => onMarkerPress(location)}>
+                            <Callout>
+                                <View style={{pading:5}}>
+                                    <Text>{location.name}</Text>
+                                </View>
+                            </Callout>
+                        </Marker>
+                    ))} */}
+                    {/* Callout (part of library) */}
+                    {Locations.map((location, index) => (
+                        <Marker key={index} coordinate={location.coordinate}>
+                            <Callout onPress={() => onMarkerPress(location)}>
+                                <View style={{ pading: 5 }}>
+                                    <Text>{location.name}</Text>
+                                </View>
+                            </Callout>
+                        </Marker>
+                    ))}
+                </MapView>
+            </View>
+        )
+    } else {
+        <View style={styles.container}>
+            <Text style={styles.title}>{current_view}</Text>
+        </View>
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'lightblue'
+    },
+    title: {
+        fontSize: 20,
+        color: 'black',
+        marginBottom: 20
+    },
+    map: {
+        width: '80%',
+        height: '80%'
+    }
+})
---
+++ locations.js
@@ -0,0 +1,14 @@
+export const Locations = [
+    {
+        name: "Queens Street Station",
+        coordinate: { latitude: 55.862644, longitude: -4.251062 }
+    },
+    {
+        name: "Central Station",
+        coordinate: { latitude: 55.859138, longitude: -4.257376 }
+    },
+    {
+        name: "BJSS Glasgow",
+        coordinate: { latitude: 55.862621, longitude: -4.258677 }
+    }
+]
